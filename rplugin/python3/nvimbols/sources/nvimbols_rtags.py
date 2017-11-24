96810eec6141db4afca219741aee2ab6d446cf0e
Jonas Bucher <j.bucher.mn@gmail.com>
2017-11-25 00:39:12 +0100

    Update NVimbols

--- rplugin/python3/nvimbols/sources/nvimbols_rtags.py
+++ rplugin/python3/nvimbols/sources/nvimbols_rtags.py
@@ -1,10 +1,36 @@
 from rtags.util import log
 from nvimbols.source.base import Base
 from nvimbols.symbol import Symbol, SymbolLocation
-from nvimbols.reference import TargetRef
-from rtags.nvimbols.rtags_symbol import RTagsSymbol
+from nvimbols.reference import TargetRef, ParentRef
 from rtags.rc import rc_get_referenced_symbol_location, rc_get_symbol_info, rc_get_referenced_by_symbol_locations
 
+
+def get_location(data):
+    location = data['location']
+    length = data['symbolLength']
+
+    tmp = location.split(':')
+    filename = tmp[0]
+    start_line = int(tmp[1])
+    start_col = int(tmp[2])
+
+    return filename, start_line, start_col, start_line, start_col + length
+
+
+class RTagsSymbol(Symbol):
+    def __init__(self, symbol):
+        Symbol.__init__(self)
+
+        self.name = symbol['symbolName']
+        self.data['Kind'] = symbol['kind']
+        self.data['Type'] = symbol['type'] if 'type' in symbol else None
+
+        if 'parent' in symbol:
+            parents = self.get_source_of(ParentRef)
+            parent_location = SymbolLocation(*get_location(symbol['parent']))
+            parents.reset()
+            parents += [parent_location]
+            parents.loaded()
 
 
 class Source(Base):
@@ -13,17 +39,14 @@ class Source(Base):
         self.name = "RTags"
         self.filetypes = ['c', 'cpp', 'objc', 'objcpp']
 
-    def _find_references(self, symbol):
+    def _find_references(self, location):
 
-        def try_find_ref(symbol):
-            referenced_location = rc_get_referenced_symbol_location(symbol.location)
+        def try_find_ref(location):
+            referenced_location = rc_get_referenced_symbol_location(location)
             if(referenced_location is not None):
-                symbol = rc_get_symbol_info(SymbolLocation(*referenced_location))
-                return RTagsSymbol(symbol) if symbol is not None else None
-            else:
-                return None
+                return SymbolLocation(*referenced_location)
 
-        cur = symbol
+        cur = location
         refs = []
         for i in range(10):
             ref = try_find_ref(cur)
@@ -33,56 +56,76 @@ class Source(Base):
             cur = ref
 
             loop = False
-            for r in [symbol] + refs:
-                if(r.location == ref.location):
+            for r in [location] + refs:
+                if(r == ref or ref.contains(r) or r.contains(ref)):
                     loop = True
                     break
             if loop:
                 break
             else:
                 refs += [ref]
-                yield ref
 
+        return refs, False
 
-    def _find_referenced_by(self, symbol):
-        # TODO! Do sth useful with incomplete
-        referenced_by_locations, incomplete = rc_get_referenced_by_symbol_locations(symbol.location)
+    def _find_referenced_by(self, location):
+        referenced_by_locations, incomplete = rc_get_referenced_by_symbol_locations(location)
         if(referenced_by_locations is None):
-            return []
+            return [], False
 
-        for location in referenced_by_locations:
-            referenced_by_symbol = rc_get_symbol_info(SymbolLocation(*location))
-            if referenced_by_symbol is None:
-                continue
-            referenced_by_symbol = RTagsSymbol(referenced_by_symbol)
+        res = []
+        for loc in referenced_by_locations:
+            ref = SymbolLocation(*loc)
 
-            if referenced_by_symbol.location == symbol.location:
+            if(ref == location or location.contains(ref) or ref.contains(location)):
                 continue
 
-            yield referenced_by_symbol
+            res += [ref]
 
-    def symbol_at_location(self, location):
+        return res, incomplete
+
+    def load_symbol(self, location):
         symbol = rc_get_symbol_info(location)
 
         if(symbol is None):
-            return None
+            location.symbol.set(None)
+        else:
+            filename, start_line, start_col, end_line, end_col = get_location(symbol)
 
-        symbol = RTagsSymbol(symbol)
+            location.filename = filename
+            location.start_line = start_line
+            location.start_col = start_col
+            location.end_line = end_line
+            location.end_col = end_col
 
-        return symbol
+            location.symbol.set(RTagsSymbol(symbol))
 
     def load_source_of(self, symbol, reference):
+        res = symbol.get_source_of(reference)
+
         if(reference == TargetRef):
-            return self._find_references(symbol)
+            refs, incomplete = self._find_references(symbol.location)
+
+            res.reset()
+            for location in refs:
+                res += [location]
+            res.loaded(incomplete)
+
         else:
-            return []
+            res.loaded(True)
 
     def load_target_of(self, symbol, reference):
-        if(reference == TargetRef):
-            return self._find_referenced_by(symbol)
-        else:
-            return []
+        res = symbol.get_target_of(reference)
 
+        if(reference == TargetRef):
+            refs, incomplete = self._find_referenced_by(symbol.location)
+
+            res.reset()
+            for location in refs:
+                res += [location]
+            res.loaded(incomplete)
+
+        else:
+            res.loaded(True)
 
 
 
